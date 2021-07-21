a7a1be408a8ea99e83997953a6a58fae43e8694f
banban <butenko.andrew@gmail.com>
2021-07-21 19:56:45 +0930

    formatting

--- bot1.py
+++ bot1.py
@@ -4,7 +4,7 @@ import os
 import logging
 import random
 
-from telegram import (InlineKeyboardMarkup, InlineKeyboardButton, Update, MessageEntity, InlineQueryResultGame) #,Game, InputTextMessageContent
+from telegram import (InlineKeyboardMarkup, InlineKeyboardButton, Update)
 from telegram.ext import (
     Updater,
     CommandHandler,
@@ -28,18 +28,18 @@ TRADER, MINER, TRADING, MINING, PAGE_MASSAGES = map(chr, range(5))
 # Shortcut for ConversationHandler.END
 END = ConversationHandler.END
 
-
 class Bot:
-    # instance members
-    #constractor
+    # constractor
     def __init__(self):
-        self.games =['MinerVsTrader']
-        self.title = 'Python-telegram-bot Test Game'
-        self.description = 'description'
-        self.photo = [{'width': 640, 'height': 360, 'file_id': 'Blah', 'file_size': 0}]
-        self.text = 'Other description'
-        self.text_entities = [{'offset': 13, 'length': 17, 'type': MessageEntity.URL}]
-        self.animation = {'file_id': 'Blah'}
+        self.games = ['MinerVsTrader']
+        #list of games to play randomly
+        self.html5_games = [
+            "https://yenots.itch.io/crypto-catch"
+            , "https://cdno.itch.io/flappy-coin"
+            , "https://bitcoinsv.itch.io/bitcoin-versus-crypto"
+            , "https://playcurious.games/games/blockchain-battle/"
+            , "https://playcurious.games/games/blockchain-battle/"
+        ]
 
     def stop(self, update: Update, context: CallbackContext) -> int:
         """End Conversation by command."""
@@ -47,131 +47,107 @@ class Bot:
         #self.remove_page_messages(update,  context)
         if bool(update.callback_query):
             return update.callback_query.message.reply_text('🏁Okay, bye')
-            #context.chat_data[PAGE_MASSAGES].append(bye_message.message_id)
+            # context.chat_data[PAGE_MASSAGES].append(bye_message.message_id)
         elif bool(update.message):
             return update.message.reply_text('🏁Okay, bye')
-            #context.chat_data[PAGE_MASSAGES].append(bye_message.message_id)
-        
+            # context.chat_data[PAGE_MASSAGES].append(bye_message.message_id)
+
         self.remove_page_messages(update, context)
-        #return END
+        # return END
 
     def remove_page_messages(self, update: Update, context: CallbackContext):
-        #print("remove_page_messages")
+        # print("remove_page_messages")
         try:
             _chat_id = update.callback_query.message.chat_id
         except:
             _chat_id = update.message.chat_id
-
-        #print("remove_page_messages: 2")
         chat_data = context.chat_data
         if PAGE_MASSAGES in chat_data and _chat_id:
-            #print("remove_page_messages: 3")
             for _id in chat_data[PAGE_MASSAGES]:
                 try:
-                    context.bot.delete_message(chat_id=_chat_id, message_id=_id)
+                    context.bot.delete_message(
+                        chat_id=_chat_id, message_id=_id)
                 except:
                     pass
         chat_data[PAGE_MASSAGES] = []
 
     def start(self, update: Update, context: CallbackContext):
-        #print("user is started")
+        """Send a message when the command /start is issued."""
+        #print("user entered")
         if bool(update.callback_query):
-            #print('start update.callback_query')
             if update.callback_query.data == str(END):
                 return self.stop(update, context)
             if update.callback_query.data == str(TRADER):
                 return update.callback_query.message.reply_text('Good luck in trading!')
             if update.callback_query.data == str(MINER):
                 return update.callback_query.message.reply_text('Good luck in mining!')
-            # if (update.callback_query.data is None):
-            #     context.chat_data[PAGE_MASSAGES] = []
-            #     return self.game(update, context)
         elif bool(update.message):
-            #print('start update.message')
-            #game = Game(title=self.title,description=self.description,photo=self.photo,text=self.text,text_entities=self.text_entities,animation=self.animation)
-            replay_game=update.message.reply_game("MinerVsTrader")
+            replay_game = update.message.reply_game(self.games[0])
             try:
-                #return the score of the specified user and several of their neighbors in a game.
+                # return the score of the specified user and several of their neighbors in a game.
                 scores = context.bot.get_game_high_scores(chat_id=replay_game.chat_id,
-                            message_id=replay_game.message_id,
-                            user_id = update.message.from_user.id)
-                _text = "Here is top list:"
+                                                          message_id=replay_game.message_id,
+                                                          user_id=update.message.from_user.id)
+                _text = "Here is top list of players:"
                 for score in scores:
-                    _text  += f"\n No: {score.position}, User: {score.user.first_name +' '+ score.user.last_name}, Score: {score.score}"
-                update.message.reply_text(text = _text)
+                    _text += f"\n No: {score.position}, User: {score.user.first_name +' '+ score.user.last_name}, Score: {score.score}"
+                update.message.reply_text(text=_text)
             except:
                 pass
             return replay_game
-        """Send a message when the command /start is issued."""
-        #update.message.reply_text('Hello, play games!')
 
-
-    def shuffleDish(self):
+    def play_shuffle(self, update: Update, context: CallbackContext):
+        """Play shuffle dish game mixing A and B randomly"""
+        #print("play_shuffle is started")
         A = ['APPLE', 'PUMKIN', 'STRAWBERRY', 'BANANA', 'CHOCOLATE']
         B = ['PIE', 'ICE-CREAM', 'CAKE', 'SHAKE']
         dish = random.choice(A)+" "+random.choice(B)
-        return dish
-
-    def play_shuffle(self, update: Update, context: CallbackContext):
-        #print("play_shuffle is started")
-        """Send a message when the command /play is issued."""
-        update.message.reply_text(self.shuffleDish())
-
-    # def inlinequery(self, update: Update, context: CallbackContext):
-    #     #print("inlinequery")
-    #     query = context.inline_query.query
-    #     results = []
-    #     for game in self.games:
-    #         if query.lower() in game.lower():
-    #             results.append(InlineQueryResultGame(id=str('uuid4()'),game_short_name=game))
-    #     context.inline_query.answer(results)
+        update.message.reply_text(dish)
 
     def play_button(self, update: Update, context: CallbackContext):
-        #print("play button")
-        #print(update)
+        #print("play button pressed")
         query = update.callback_query
-        #admin mode
+        
+        # admin mode
         if query.message:
             buttons = [
                 [
-                    InlineKeyboardButton(text='Trader', callback_data=str(TRADER)),
-                    InlineKeyboardButton(text='Miner', callback_data=str(MINER)),
+                    InlineKeyboardButton(
+                        text='💰Trader', callback_data=str(TRADER)),
+                    InlineKeyboardButton(
+                        text='⛏Miner', callback_data=str(MINER)),
                 ],
                 [
-                    InlineKeyboardButton(text=f"🔎Trading", callback_data=str(TRADING)),
-                    InlineKeyboardButton(text=f"⛓Mining", callback_data=str(MINING)),
+                    InlineKeyboardButton(
+                        text=f"📉Trading", callback_data=str(TRADING)),
+                    InlineKeyboardButton(
+                        text=f"⛓Mining", callback_data=str(MINING)),
                 ],
                 [
                     InlineKeyboardButton(text='⏹Exit', callback_data=str(END)),
                 ],
             ]
-            keyboard = InlineKeyboardMarkup(buttons) #, reply_markup=keyboard
+            keyboard = InlineKeyboardMarkup(buttons)  # , reply_markup=keyboard
             return query.message.reply_text(text=f"Please choose your role...", reply_markup=keyboard)
 
-        #user group mode
+        # user mode in group
         if query.inline_message_id:
             game = query.game_short_name
             ilmid = query.inline_message_id
             uid = query.from_user.id
 
-            #grant player random score
+            # grant player random score
             try:
                 context.bot.set_game_score(
-                    inline_message_id = ilmid,
-                    user_id = uid,
-                    score=random.randint(0,100),
-                    force = True)
+                    inline_message_id=ilmid,
+                    user_id=uid,
+                    score=random.randint(0, 100),
+                    force=True)
             except:
                 pass
 
-            #redirect to random game
-            url = random.choice([
-                    "https://yenots.itch.io/crypto-catch"
-                    ,"https://cdno.itch.io/flappy-coin"
-                    ,"https://bitcoinsv.itch.io/bitcoin-versus-crypto"
-                    ,"https://playcurious.games/games/blockchain-battle/"
-                    ,"https://playcurious.games/games/blockchain-battle/"
-                ])
+            # redirect to random game
+            url = random.choice(self.html5_games)
             return context.bot.answer_callback_query(query.id, text=game, url=url)
 
         # if query.message:
@@ -201,7 +177,7 @@ class Bot:
     def help(self, update: Update, context: CallbackContext):
         #chat_id = update.message.chat_id
         message_id = update.message.message_id
-        reply=update.message.reply_text(
+        reply = update.message.reply_text(
             "Type one of the following commands:"
             "\n/start - initiate guided session"
             "\n/stop - terminate conversation"
@@ -223,20 +199,14 @@ class Bot:
         updater = Updater(C.TELEGRAM_TOKEN)
         # Get the dispatcher to register handlers
         dp = updater.dispatcher
-        
-        dp.add_handler(CommandHandler("start", self.start))
         dp.add_handler(CommandHandler("help", self.help))
+        dp.add_handler(CommandHandler("start", self.start))
         dp.add_handler(CommandHandler('stop', self.stop))
         dp.add_handler(CommandHandler("shuffle", self.play_shuffle))
-
-        dp.add_handler(CallbackQueryHandler(self.start, pattern='^' + str(TRADER) + '|'+ str(MINER) + '|'+ str(END) + '$')) #End button
+        dp.add_handler(CallbackQueryHandler(self.start, pattern='^' +
+                       str(TRADER) + '|' + str(MINER) + '|' + str(END) + '$'))
         dp.add_handler(CallbackQueryHandler(self.play_button))
-
-        #dp.add_handler(CallbackQueryHandler(Filters.game, self.start, pattern=r'.*'))  #^' + str(PLAY) + '$
-        #dp.add_handler(CallbackQueryHandler(self.game, pattern=r'.*'))  #, pattern=r'.*') ^' + str(PLAY) + '$
-        #dp.add_handler(InlineQueryHandler(self.inlinequery)) #, pattern=Filters.regex('^(File|Coupon|Other)$')
-        #updater.dispatcher.add_handler(InlineQueryHandler(inlinequery))
-        #updater.dispatcher.add_handler(CallbackQueryHandler(button))
+        dp.add_error_handler(self.error)
 
         # # Callback handlers
         # hit_callback_handler = CallbackQueryHandler(game.hit_callback, pattern=r"^hit_[0-9]{7}$")
@@ -245,26 +215,19 @@ class Bot:
         # start_callback_handler = CallbackQueryHandler(game.start_callback, pattern=r"^start_[0-9]{7}$")
         # newgame_callback_handler = CallbackQueryHandler(game.newgame_callback, pattern=r"^newgame$")
         # language_callback_handler = CallbackQueryHandler(settings.language_callback, pattern=r"^lang_([a-z]{2}(?:-[a-z]{2})?)$")
-
         # handlers = [banned_user_handler,
         #             start_command_handler, stop_command_handler, join_callback_handler, hit_callback_handler,
         #             stand_callback_handler, start_callback_handler, language_command_handler, stats_command_handler,
         #             newgame_callback_handler, reload_lang_command_handler, language_callback_handler, users_command_handler,
         #             comment_command_handler, comment_text_command_handler, answer_command_handler, ban_command_handler,
         #             unban_command_handler, bans_command_handler]
-
         # __all__ = ['handlers', 'error_handler']
 
-        # on noncommand i.e message - echo the message on Telegram
-        #dp.add_handler(MessageHandler(Filters.text, self.play))
-        dp.add_error_handler(self.error)
-
-
         # Run bot
-        if C.HEROKU_APP_NAME == "":  #pooling mode
+        if C.HEROKU_APP_NAME == "":  # pooling mode
             #logger.info("Can't detect 'HEROKU_APP_NAME' env. Running bot in pooling mode.")
             updater.start_polling(1)
-        else:  #webhook mode
+        else:  # webhook mode
             PORT = int(os.environ.get('PORT', C.HEROKU_PORT))
             updater.start_webhook(
                 listen="0.0.0.0",
@@ -277,6 +240,7 @@ class Bot:
         # SIGTERM or SIGABRT. This should be used most of the time, since
         # start_polling() is non-blocking and will stop the bot gracefully.
         updater.idle()
+
 
 if __name__ == '__main__':
     bot = Bot()
